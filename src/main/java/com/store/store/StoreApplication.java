50af44d7300553b5661a427a528684a452ef252e
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-10-20 13:38:27 +0200

    Remove @EnableJpaAuditing(auditorAwareRef = auditorAwareImpl) and add to this public class JpaConfig

--- src/main/java/com/store/store/StoreApplication.java
+++ src/main/java/com/store/store/StoreApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 @EnableCaching
 @EnableConfigurationProperties(value = {ContactInfoDto.class})
 public class StoreApplication {
